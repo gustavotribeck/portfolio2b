b54802ffe84de8441447e01e75c59899b19431f2
gustavotribeck <102532312+gustavotribeck@users.noreply.github.com>
2022-04-11 10:46:28 -0300

    autoria

---
+++ Atividade Números Primos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner bofao = new Scanner(System.in);
+		System.out.println("Digite um numero");
+	    int n = bofao.nextInt();
+	    ubt i = 0;
+	     while(i < n){
+	        int j = i;
+	        int d = 0;
+	        while(i % j == 0)
+	        if(i % j -- 0){
+	        d++;
+	        }
+	        j--;
+	     }
+	     if(d == 2){
+	         system.out.println(i);
+	     }
+	         i ++;
+	   
+	     
+	       }
+	    }
+	    
+	}
+}
